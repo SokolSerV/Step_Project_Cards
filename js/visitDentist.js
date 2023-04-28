fcbb9a1091d5df2b1cc43ddceb8f308a51d3512b
Сергій Сокол <sokol.serhii@gmail.com>
2023-04-29 01:02:29 +0300

    Created page

--- js/visitDentist.js
+++ js/visitDentist.js
@@ -1,17 +1,1 @@
-import { formVisitInfoWrapper } from "./constants.js";
-import Visit from "./Visit.js";
-
-export default class VisitDentist extends Visit {
-    renderDentistInputs() {
-        const inputLastVisitDate = this.createInput(
-            "date_last_visit",
-            "Date last visit",
-            "01-01-2000"
-        );
-
-        formVisitInfoWrapper.append(
-            ...this.createBaseInputs(),
-            inputLastVisitDate
-        );
-    }
-}
+import{formVisitInfoWrapper}from"./constants.js";import Visit from"./Visit.js";export default class VisitDentist extends Visit{renderDentistInputs(){var t=this.createInput("date_last_visit","Date last visit","01-01-2000");formVisitInfoWrapper.append(...this.createBaseInputs(),t)}}
--- js/visitTherapist.js
+++ js/visitTherapist.js
@@ -1,10 +1,1 @@
-import { formVisitInfoWrapper } from "./constants.js";
-import Visit from "./Visit.js";
-
-export default class VisitTherapist extends Visit {
-    renderTherapistInputs() {
-        const inputAge = this.createInput("age", "Age", "18");
-
-        formVisitInfoWrapper.append(...this.createBaseInputs(), inputAge);
-    }
-}
+import{formVisitInfoWrapper}from"./constants.js";import Visit from"./Visit.js";export default class VisitTherapist extends Visit{renderTherapistInputs(){var t=this.createInput("age","Age","18");formVisitInfoWrapper.append(...this.createBaseInputs(),t)}}
